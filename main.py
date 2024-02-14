0579c5353d00652d11f4497d2609217128e7ea87
Daniel W <tildaandpumpkins@gmail.com>
2024-02-14 12:38:32 +0300

    Update main.py
    Accounted for multiple numbers that have max sum of digits

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 n = int(input('Enter a number: '))
-max_sum_num = 0
+max_sum_nums = [0]
 max_sum = 0
 while not (n==0):
     cur_sum = sum([int(d) for d in str(n).replace('-', '')])
     if (cur_sum > max_sum):
-        max_sum_num = n
+        max_sum_nums = [n]
         max_sum = cur_sum
+    elif (cur_sum == max_sum):
+        max_sum_nums.append(n)
     n = int(input('Enter a number: '))
 
-print(f'The number {max_sum_num} has the largest sum of digits.\nThe sum of its digits is {max_sum}. ')
+print(f'The max sum digits among input numbers is {max_sum}.')
+print('The following numbers have this sum of digits:')
+for num in max_sum_nums:
+    print(f'{num}', end=' ')
+print()
