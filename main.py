a9cf4dcc587116e91960d4db55de07b738ef18c4
Daniel W <tildaandpumpkins@gmail.com>
2024-02-14 12:04:19 +0300

    Update main.py
    Changed a variable name for better readability

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 n = int(input('Enter a number: '))
-max_num = n
+max_sum_num = 0
 max_sum = 0
 while not (n==0):
     cur_sum = sum([int(d) for d in str(n)])
     if (cur_sum > max_sum):
-        max_num = n
+        max_sum_num = n
         max_sum = cur_sum
     n = int(input('Enter a number: '))
 
-print(f'The number {max_num} has the largest sum of digits.\nThe sum of its digits is {max_sum}. ')
+print(f'The number {max_sum_num} has the largest sum of digits.\nThe sum of its digits is {max_sum}. ')
