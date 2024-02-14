14f2f92c1759b4dc2b67e0054b5eebdf642541af
Daniel W <tildaandpumpkins@gmail.com>
2024-02-14 14:32:49 +0300

    Update main.py
    Added input validity check

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-n = int(input('Enter a number: '))
+import re
+
+expected_number_pattern = re.compile("^-?[0-9]+$")
+    
+def read_input():
+    valid_input = False
+    while not valid_input:
+        s = input('Enter an integer: ' )
+        if (re.match(expected_number_pattern, s)):
+            n = int(s)
+            valid_input = True
+        else:
+            print('Invalid input!')
+    return n
+
 max_sum_nums = [0]
 max_sum = 0
+n = read_input()
 while not (n==0):
     cur_sum = sum([int(d) for d in str(n).replace('-', '')])
     if (cur_sum > max_sum):
@@ -8,7 +23,7 @@ while not (n==0):
         max_sum = cur_sum
     elif (cur_sum == max_sum):
         max_sum_nums.append(n)
-    n = int(input('Enter a number: '))
+    n = read_input()
 
 print(f'The max sum digits among input numbers is {max_sum}.')
 print('The following numbers have this sum of digits:')
